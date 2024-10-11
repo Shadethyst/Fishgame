c9547f7b08b7816939611f61612183f5bd902e87
Lassi Kimppa <Lassi.kimppa@gmail.com>
2024-10-11 11:45:33 +0300

    changed player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    GameObject player;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
