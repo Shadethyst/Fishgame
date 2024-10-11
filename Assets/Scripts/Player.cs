a1accd6570f3cb3eb0f5986cf02a078573cd070f
Lassi Kimppa <Lassi.kimppa@gmail.com>
2024-10-11 13:40:49 +0300

    Added unity input system, continued movement script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private GameObject player;
+    private float acceleration;
+    private float maxSpeed;
+    private float speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +22,11 @@ public class Player : MonoBehaviour
     {
         
     }
+    // FixedUpdate is called 50 times a second on a fixed interval
     private void FixedUpdate()
     {
-        
+        if (speed < maxSpeed) {
+            speed = acceleration * Time.fixedDeltaTime;
+                }
     }
 }
